6152a15aa57c602e53248cc2639f58ed99d37e91
Githerdone <GitHub@JakeDanforth.com>
2013-12-23 23:11:35 -0700

    devise gem working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uueec::Application.config.secret_key_base = '198c203c7a1165497c9315c702c1ed9d7b7521b0b7f6fec7316fa933ee1250dd69db1c93527515e69aca252e69429a0b1ce6fb37d2e0ccdd6282542e667f6324'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Uueec::Application.config.session_store :cookie_store, key: '_uueec_session'
