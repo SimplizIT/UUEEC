6ebd64bbb1490c9a9f570d691a78fe3d4e8aa146
Githerdone <GitHub@JakeDanforth.com>
2013-12-30 14:44:06 -0700

    working role search field and disable admin for current user

--- app/assets/javascripts/roles.js
+++ app/assets/javascripts/roles.js
@@ -1,8 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+var roleSearch = function(){
+  var $roles = $('#role_table_body tr');
+  $('#role_search').keyup(function() {
+    var re = new RegExp($(this).val(), "i"); // "i" means it's case-insensitive
+    $roles.show().filter(function() {
+        return !re.test($(this).find('td').text());
+    }).hide();
+  });
+}
 
-
-
-  $("#roletable").tablesorter();
-          $("#myTable").tablesorter(); 
+$(document).ready(roleSearch);
+$(document).on('page:load', roleSearch);
--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,8 +1,15 @@
 module RolesHelper
   def sortable(column, title = nil)
     title ||= column.titleize
-    css_class = column == sort_column ? 'current #{sort_direction}' : nil
+    css_class = column == sort_column ? 'current ' + sort_direction : nil
+    if column == sort_column && sort_direction == 'asc'
+      css_icon = 'fa fa-chevron-down'
+    elsif column == sort_column && sort_direction == 'desc'
+      css_icon = 'fa fa-chevron-up'
+    else
+      css_icon = nil
+    end
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
-    link_to title, {sort: column, direction: direction}, {class: css_class}
+    content_tag(:i, link_to(title, {sort: column, direction: direction}, {class: css_class}), class: css_icon)
   end
 end
