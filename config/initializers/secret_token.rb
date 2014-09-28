26ea0601cf16e4d517caa095eff0308db467f202
Githerdone <GitHub@JakeDanforth.com>
2014-09-28 11:18:46 -0700

    added security flavor

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Uueec::Application.config.secret_key_base = '198c203c7a1165497c9315c702c1ed9d7b7521b0b7f6fec7316fa933ee1250dd69db1c93527515e69aca252e69429a0b1ce6fb37d2e0ccdd6282542e667f6324'
+Uueec::Application.config.secret_key_base = 'f15eee3063711dd71079ddcac7f8618dfd198e6d972f3e787008cedc1e092f34fb2fc02b54b78a0807d3d6b20f69bc6857dec11a93b96b66d103199c2082f9f0'
