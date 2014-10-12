05600fe05131a777087483aaf7cb2fdadd7eccf3
revspringjake <jdanforth@revspringinc.com>
2014-10-12 10:46:37 -0700

    settig production env mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Uueec::Application.configure do
     port: 587,
     domain: 'heroku.com',
     authentication: :plain,
-    user_name: Rails.application.secrets.email_name,
-    password: Rails.application.secrets.email_password,
+    user_name: ENV['MANDRILL_USERNAME'],
+    password: ENV['MANDRILL_APIKEY'],
   }
   ActionMailer::Base.delivery_method = :smtp
 
